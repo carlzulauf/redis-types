e0f597f61b15c60962df9d8b833c5e82e67c323e
Carl Zulauf <carl@linkleaf.com>
2012-09-04 02:35:06 -0400

    Remove fail strategy and improve Redis::Types.load

--- lib/redis/types.rb
+++ lib/redis/types.rb
@@ -14,7 +14,6 @@ require 'redis/types/hash_map/track_changes'
 require 'redis/types/hash_map/strategies/merge'
 require 'redis/types/hash_map/strategies/change'
 require 'redis/types/hash_map/strategies/lock'
-require 'redis/types/hash_map/strategies/fail'
 
 require 'redis/types/big_hash'
 
@@ -23,12 +22,19 @@ class Redis
     class << self
       def load(key, options = {})
         redis = options[:redis] || Redis.current
+        type  = options.delete(:type).to_s
         case redis.type( key )
         when "hash"
-          if options[:type].to_s =~ /hash_map/ or redis.object(:encoding, key) == "zipmap"
-            HashMap.new(key, :redis => redis)
+          if type =~ /big_hash/
+            BigHash.new(key, options)
+          elsif type =~ /hash_map/
+            HashMap.new(key, options)
           else
-            BigHash.new(key, :redis => redis)
+            if redis.object(:encoding, key) == "zipmap"
+              HashMap.new(key, options)
+            else
+              BigHash.new(key, options)
+            end
           end
         when "string"
           Marshal.load redis.get( key )
--- lib/redis/types/hash_map/strategies/fail.rb
+++
@@ -1,5 +0,0 @@
-# Raises an error when attempt is made to save a hash
-# that has been edited concurrently.
-module Redis::Types::HashMap::Strategies::Fail
-  # TODO
-end
