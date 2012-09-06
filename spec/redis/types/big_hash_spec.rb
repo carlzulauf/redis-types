472d8645dcdedeeba53d6716c40e4d5d824df055
Carl Zulauf <czulauf@lyconic.com>
2012-09-06 02:05:56 -0400

    Add marshal tests

--- spec/redis/types/big_hash_spec.rb
+++ spec/redis/types/big_hash_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "Redis::Types::BigHash" do
+  
   before :each do
     @hash = Redis::Types::BigHash.new("test")
     @hash[:foo] = "bar"
   end
+
   describe "#[]" do
     it "should read existing values" do
       @hash[:foo].should == "bar"
@@ -13,12 +15,24 @@ describe "Redis::Types::BigHash" do
       @hash["foo"].should == "bar"
     end
   end
+
   describe "#[]=" do
     it "should write values" do
       @hash[:yin] = "yang"
       @hash[:yin].should == "yang"
     end
+    it "should marshal objects" do
+      class Foo; attr_accessor :test; end
+      @hash[:key]   = Foo.new.tap{|foo| foo.test = "bar" }
+      @hash[:int]   = 12_345
+      @hash[:float] = 12.345
+      @hash[:key].should      be_a(Foo)
+      @hash[:key].test.should == "bar"
+      @hash[:int].should      === 12_345
+      @hash[:float].should    === 12.345
+    end
   end
+
   describe "#each" do
     it "should iterate through a hash with one value" do
       @hash.each do |key,value|
@@ -35,6 +49,7 @@ describe "Redis::Types::BigHash" do
       end
     end
   end
+
   describe "#find" do
     it "should find existing keys" do
       @hash.find{|k,v| k == "foo" }.should == ["foo", "bar"]
@@ -43,6 +58,7 @@ describe "Redis::Types::BigHash" do
       @hash.find{|k,v| k == "something" }.should be_nil
     end
   end
+
   after :each do
     @hash.destroy
   end
