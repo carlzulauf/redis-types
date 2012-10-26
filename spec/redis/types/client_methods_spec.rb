9399ee7e93561c12f7c4efae94ce7a3992b1d948
Carl Zulauf <carl@linkleaf.com>
2012-10-26 19:50:30 -0400

    Remove 1.9 specific hash syntax from ClientMethods specs

--- spec/redis/types/client_methods_spec.rb
+++ spec/redis/types/client_methods_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Redis::Types::ClientMethods" do
   describe "#new" do
     it "should allow a namespace to be specified" do
-      t = Redis::Types::Hash.new(namespace: "test")
+      t = Redis::Types::Hash.new(:namespace => "test")
       t.namespace.should == "test"
     end
     it "should allow a key to be specified" do
@@ -11,13 +11,13 @@ describe "Redis::Types::ClientMethods" do
       t.key.should == "test"
     end
     it "should allow a key and namespace to be specified" do
-      t = Redis::Types::Hash.new(key: "foo", namespace: "test")
+      t = Redis::Types::Hash.new(:key => "foo", :namespace => "test")
       t.key.should == "foo"
       t.namespace.should == "test"
     end
     it "should allow a redis client to be provided" do
-      redis = Redis.new(db: 5)
-      t = Redis::Types::Hash.new(redis: redis)
+      redis = Redis.new(:db => 5)
+      t = Redis::Types::Hash.new(:redis => redis)
       t.redis.client.db.should == 5
     end
   end
